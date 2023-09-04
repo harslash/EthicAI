3db367ac576a973aef9562326bbffe042d1b55ce
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-09-05 03:01:07 +1200

    Created footer component

---
+++ project/ethicai-spa/src/components/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Footer',
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+});
+</script>
+
+<style lang="css" scoped>
+
+</style>
