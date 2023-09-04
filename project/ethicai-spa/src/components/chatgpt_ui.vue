eb63d256d1640b56f2937ed279a6f994a9dee233
georgiapurkiss <104548310+georgiapurkiss@users.noreply.github.com>
2023-09-04 17:39:11 +1200

    Create chatgpt_ui.vue

---
+++ project/ethicai-spa/src/components/chatgpt_ui.vue
@@ -0,0 +1,36 @@
+<body>
+    <div id="app">
+        <div class="card">
+            <div class="rectangle">
+                <div class="icon-container">
+                    <div class="circle">
+                        <img src="pics/smiley_icon.png">
+                    </div>
+                </div>
+                <div class="text-container">
+                    {{ question }} <!-- Bind to the question property -->
+                </div>
+            </div>
+            <div class="rectangle">
+                <div class="icon-container">
+                    <div class="circle">
+                        <img src="pics/robot_icon.png">
+                    </div>
+                </div>
+                <div class="text-container">
+                    {{ answer }} <!-- Bind to the answer property -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        new Vue({
+            el: '#app',
+            data: {
+                question: 'What is your question?', // Predefined input question
+                answer: 'This is the answer.', // Predefined output answer
+            },
+        });
+    </script>
+</body>
