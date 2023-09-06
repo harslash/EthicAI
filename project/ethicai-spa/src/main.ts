5c10016b964b05d0ae3e98acb50cac38854bd6b6
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-09-06 23:31:53 +1200

    Registered tool tip component from PrimeVue

--- project/ethicai-spa/src/main.ts
+++ project/ethicai-spa/src/main.ts
@@ -4,7 +4,8 @@ import App from './App.vue';
 import router from './router'
 
 //styling
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'primevue/resources/themes/saga-blue/theme.css'; // Choose a theme
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
@@ -15,17 +16,21 @@ const app = createApp(App).use(router);
 app.use(PrimeVue);
 app.use(router)
 
+//Vue components
 import Button from "primevue/button"
 import Slider from 'primevue/slider';
 import Dialog from 'primevue/dialog';
 import MenuBar from "primevue/menubar"
+import Tooltip from 'primevue/tooltip';
 
-
-
+//registering components
 app.component('prime-button', Button);
 app.component('prime-slider', Slider);
 app.component('prime-dialog', Dialog);
 app.component('MenuBar', MenuBar);
+
+app.directive('tooltip', Tooltip);
+
 
 
 app.mount('#app');
