35af3bd2e84931ec252238ba584c53b4c84e55b4
georgiapurkiss <104548310+georgiapurkiss@users.noreply.github.com>
2023-09-07 22:29:37 +0000

    edit references page

--- project/ethicai-spa/src/components/pages/references-page.vue
+++ project/ethicai-spa/src/components/pages/references-page.vue
@@ -2,27 +2,57 @@
     <nav-bar />
     <div class="container-fluid page-container text-center">
         <div class="row h-25">
-           <h1><u> References </u> </h1>
+            <h1 class="main-heading"><u> References </u> </h1>
         </div>
         <div class="row">
-            <!-- Add seven full-width gray rectangles here -->
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <h2 class="section-heading">The Age of Chatbots Module</h2> <!-- Second Header -->
+            </div>
+        </div>
+        <div class="row">
+            <!-- Add empty headers labeled "text" above each rectangle -->
+            <div class="col-md-12">
+                <h3 class="sub-heading">The Age of Chatbots</h3>
             </div>
             <div class="col-md-12">
                 <div class="gray-rectangle"></div>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <h3 class="sub-heading">Too Good to be True?</h3>
             </div>
             <div class="col-md-12">
                 <div class="gray-rectangle"></div>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <h3 class="sub-heading">Hallucination</h3>
             </div>
             <div class="col-md-12">
                 <div class="gray-rectangle"></div>
+            </div>
+            <div class="col-md-12">
+                <h3 class="sub-heading">Misuses and Plagiarism</h3>
+            </div>
+            <div class="col-md-12">
+                <div class="gray-rectangle"></div>
+            </div>
+            
+            <div class="col-md-12">
+                <h2 class="section-heading">Rise of Dalle Module</h2> <!-- Second Header -->
+            </div>
+            <div class="col-md-12">
+                <h3 class="sub-heading">The Rise of DALL·E</h3>
+            </div>
+            <div class="col-md-12">
+                <div class="gray-rectangle"></div>
+            </div>
+            <div class="col-md-12">
+                <h3 class="sub-heading">AI Generated vs Human Art</h3>
+            </div>
+            <div class="col-md-12">
+                <div class="gray-rectangle"></div>
+            </div>
+            <div class="col-md-12">
+                <h3 class="sub-heading">Copyright and Copywrong</h3>
             </div>
             <div class="col-md-12">
                 <div class="gray-rectangle"></div>
@@ -60,12 +90,29 @@ export default defineComponent({
     min-height: calc(100vh - 60px); /* Set the minimum height to avoid going into the footer */
     margin-top: 60px;
     padding-bottom: 20px; /* Add some padding at the bottom for spacing */
-    padding-top: 20px;
+    padding-top: 40px;
 }
+
+.main-heading {
+    font-size: 36px;
+    font-weight: bold;
+}
+
+.section-heading {
+    text-align: left;
+    text-decoration: underline;
+    color: #6D0CFF;
+    font-size: 24px;
+}
+
+.sub-heading {
+    text-align: left;
+    font-size: 24px;
+}
+
 .gray-rectangle {
     background-color: #ccc; /* Gray color for the rectangle */
     height: 100px; /* Adjust the height as needed */
     margin: 10px 0; /* Add vertical margin for spacing between rectangles */
 }
-/* Your component-specific styles go here */
 </style>
