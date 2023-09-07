9245de9039adcbf5b41d4d302c5228185fc57374
georgiapurkiss <104548310+georgiapurkiss@users.noreply.github.com>
2023-09-07 21:33:33 +0000

    landing page changes

--- project/ethicai-spa/src/App.vue
+++ project/ethicai-spa/src/App.vue
@@ -11,8 +11,7 @@ import LoginPage from './components/pages/login-page.vue';
 import NavBar from './components/reusable/nav-bar.vue'
 import PageFooter from './components/reusable/page-footer.vue'
 import ModuleOverview from './components/reusable/module_overview.vue'
-import LandingHero from './components/pages/landing-hero.vue';
-import LandingCard from './components/pages/landing-card.vue';
+import LandingPage from './components/pages/landing-page.vue';
 import ChatGPTUI from './components/reusable/chatgpt_ui.vue';
 export default defineComponent({
   name: 'App',
@@ -21,8 +20,7 @@ export default defineComponent({
     NavBar,
     PageFooter,
     ModuleOverview,
-    LandingHero,
-    LandingCard,
+    LandingPage,
     ChatGPTUI
   }
 });
