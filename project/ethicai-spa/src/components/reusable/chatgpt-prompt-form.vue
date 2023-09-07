8a7668392db6dc70275208aa05b90fb928726791
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-09-07 16:15:06 +1200

    Created ChatGPT prompt form component

---
+++ project/ethicai-spa/src/components/reusable/chatgpt-prompt-form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Here</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ChatgptPromptForm',
+    props: {
+    },
+    methods: {
+    },
+});
+</script>
+
+<style>
+/* Add your CSS styles here, scoped to this component */
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap');
+
+</style>
