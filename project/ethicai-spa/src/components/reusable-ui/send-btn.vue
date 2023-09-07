c1012a3c054d580fe42406473f9907df7e28f8dd
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-09-07 17:24:03 +1200

    Created reusable send button

---
+++ project/ethicai-spa/src/components/reusable-ui/send-btn.vue
@@ -0,0 +1,31 @@
+<template>
+      <button class="btn btn-primary send-button" type="button">
+      <font-awesome-icon :icon="faPaperPlane" style="color: #ffffff;"></font-awesome-icon>
+      </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+
+export default defineComponent({
+    name: 'SendButton',
+    data() {
+        return {
+            faPaperPlane: faPaperPlane,
+
+        }
+    }
+});
+</script>
+
+<style scroped>
+.btn {
+    background-color: #6D0CFF;
+}
+.btn:hover {
+    background-color: #8638fc;
+}
+
+</style>>
