a91985af32a579881ba5db0b8e94afd451fc3752
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-09-07 16:00:10 +1200

    Create reusable purple button

--- project/ethicai-spa/src/components/reusable-ui/purple-btn.vue
+++ project/ethicai-spa/src/components/reusable-ui/purple-btn.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="hello">
-        <div class="justify-center flex bg-yellow-300 items-center h-screen">
-            <div class="text-4xl">
-            Hello 👋🏼
-            </div>
-        </div>
-        <prime-button label="Submit" />
-     
-    </div>
-
+    <button type="button" class="btn btn-primary ">{{text}}</button>
 </template>
 <script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'PurpleButton',
+    props: {
+        'text': String
+    },
+    methods: {
+    }
+});
 
 </script>
 
-<style>
+<style scroped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap');
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -23,5 +25,15 @@
     color: #2c3e50;
     margin-top: 60px;
 }
+.btn {
+    background-color: #6D0CFF;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 600;
+}
+
+.btn:hover {
+    background-color: #8638fc;
+}
+
 
 </style>>
