5acaea3c920aed542b8f1923e3bec1807bb60015
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-09-07 16:45:06 +1200

    Added styling to chatgpt prompt component

--- project/ethicai-spa/src/components/reusable/chatgpt-prompt-form.vue
+++ project/ethicai-spa/src/components/reusable/chatgpt-prompt-form.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>Here</div>
+  <form class="form-container d-flex justify-content-center align-items-center flex-wrap">
+      <div class="form-group">
+        <textarea 
+        class="form-control" 
+        id="exampleFormControlTextarea1" 
+        rows="3"
+        placeholder="Enter your input here"></textarea>
+      </div>
+      <div class="form-group">
+        <textarea 
+        class="form-control" 
+        id="exampleFormControlTextarea1" 
+        rows="3"></textarea>
+      </div>
+    </form>
 </template>
 
 <script lang="ts">
@@ -14,8 +28,23 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
 /* Add your CSS styles here, scoped to this component */
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap');
+.form-container {
+    border: 2px solid #E8E4E4;
+    box-shadow: 5px 5px 5px #E8E4E4;
+    border-radius: 20px;
+}
+
+.form-group {
+    width: 60%;
+}
+
+.form-control {
+    border: 2px solid black;
+     border-radius: 10px;
+}
+
 
 </style>
