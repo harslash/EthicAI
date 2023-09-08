97541b477ad6957ab9ed32cd6735db0d72c23fc8
georgiapurkiss <104548310+georgiapurkiss@users.noreply.github.com>
2023-09-08 04:46:55 +0000

    edit references

--- project/ethicai-spa/src/components/pages/references-page.vue
+++ project/ethicai-spa/src/components/pages/references-page.vue
@@ -46,19 +46,70 @@
                 <h3 class="sub-heading">Too Good to be True?</h3>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <div class="gray-rectangle">
+                <p>
+                    Cousins, B. (2023, March 31). Uncovering The Different Types Of ChatGPT Bias. Forbes. Retrieved August 30, 2023, from
+                    <a href="https://www.forbes.com/sites/forbestechcouncil/2023/03/31/uncovering-the-different-types-of-chatgpt-bias/" target="_blank">https://www.forbes.com/sites/forbestechcouncil/2023/03/31/uncovering-the-different-types-of-chatgpt-bias/</a>
+                </p>
+                <p>
+                    Guinness, H. (2023). How does ChatGPT work? Zapier. 
+                    <a href="https://zapier.com/blog/how-does-chatgpt-work/" target="_blank">https://zapier.com/blog/how-does-chatgpt-work/</a>
+                </p>
+                <p>
+                    Gupta, N. (2023). The Astonishing Amount of Data Analyzed by ChatGPT: Story Told By ChatGPT. www.linkedin.com. 
+                    <a href="https://www.linkedin.com/pulse/astonishing-amount-data-analyzed-chatgpt-story-told-nimit-gupta/" target="_blank">https://www.linkedin.com/pulse/astonishing-amount-data-analyzed-chatgpt-story-told-nimit-gupta/</a>
+                </p>
+                <p>
+                    Ray, P. (2023). ChatGPT: A comprehensive review on background, applications, key challenges, bias, ethics, limitations and future scope. Internet of Things and Cyber-physical Systems, 3, 121–154. 
+                    <a href="https://doi.org/10.1016/j.iotcps.2023.04.003" target="_blank">https://doi.org/10.1016/j.iotcps.2023.04.003</a>
+                </p>
+                <p>
+                    Ruby, D. (2023). 30+ Detailed ChatGPT Statistics — Users & Facts (August 2023). DemandSage. 
+                    <a href="https://www.demandsage.com/chatgpt-statistics/" target="_blank">https://www.demandsage.com/chatgpt-statistics/</a>
+                </p>
+                </div>
             </div>
             <div class="col-md-12">
                 <h3 class="sub-heading">Hallucination</h3>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <div class="gray-rectangle">
+                <p>
+                    Gupta, N. (2023). The Astonishing Amount of Data Analyzed by ChatGPT: Story Told By ChatGPT. www.linkedin.com. 
+                    <a href="https://www.linkedin.com/pulse/astonishing-amount-data-analyzed-chatgpt-story-told-nimit-gupta/" target="_blank">https://www.linkedin.com/pulse/astonishing-amount-data-analyzed-chatgpt-story-told-nimit-gupta/</a>
+                </p>
+                <p>
+                    OpenAI. (n.d.). Introducing ChatGPT. Retrieved August 30, 2023, from
+                    <a href="https://openai.com/blog/chatgpt" target="_blank">https://openai.com/blog/chatgpt</a>
+                </p>
+        </div>
             </div>
             <div class="col-md-12">
                 <h3 class="sub-heading">Misuses and Plagiarism</h3>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <div class="gray-rectangle">
+                <p>
+                    Alston, E. (2023). What are AI hallucinations and how do you prevent them? zapier.com.
+                    <a href="https://zapier.com/blog/ai-hallucinations/" target="_blank">https://zapier.com/blog/ai-hallucinations/</a>
+                </p>
+                <p>
+                    Cousins, B. (2023, March 31). Uncovering The Different Types Of ChatGPT Bias. Forbes. Retrieved August 30, 2023, from
+                    <a href="https://www.forbes.com/sites/forbestechcouncil/2023/03/31/uncovering-the-different-types-of-chatgpt-bias/" target="_blank">https://www.forbes.com/sites/forbestechcouncil/2023/03/31/uncovering-the-different-types-of-chatgpt-bias/</a>
+                </p>
+                <p>
+                    NSW Education. (2023, June 1). Guidelines regarding use of generative AI (ChatGPT). Retrieved August 30, 2023, from
+                    <a href="https://education.nsw.gov.au/teaching-and-learning/education-for-a-changing-world/guidelines-regarding-use-of-generative-ai-chatgpt" target="_blank">https://education.nsw.gov.au/teaching-and-learning/education-for-a-changing-world/guidelines-regarding-use-of-generative-ai-chatgpt</a>
+                </p>
+                <p>
+                    OpenAI. (2023, July 23). Privacy policy. Retrieved August 30, 2023, from
+                    <a href="https://openai.com/policies/privacy-policy" target="_blank">https://openai.com/policies/privacy-policy</a>
+                </p>
+                <p>
+                    ZeroGPT. (2023). GPT-4, ChatGPT & AI Detector by ZeroGPT: detect OpenAI text. Retrieved September 1, 2023, from
+                    <a href="https://www.zerogpt.com/" target="_blank">https://www.zerogpt.com/</a>
+                </p>
+                </div>
             </div>
             
             <div class="col-md-12">
@@ -68,19 +119,54 @@
                 <h3 class="sub-heading">The Rise of DALL·E</h3>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <div class="gray-rectangle">
+                    <p>
+                        AWS. (2023). What is GPT?<br>
+                        AWS.Amazon.com. <a href="https://aws.amazon.com/what-is/gpt/" target="_blank">https://aws.amazon.com/what-is/gpt/</a>
+                    </p>
+                    <p>
+                        OpenAI. (2023). ChatGPT. (GPT-3.5). [Large Language Model].<br>
+                        <a href="https://chat.openai.com/" target="_blank">https://chat.openai.com/</a>
+                    </p>
+                    <p>
+                        OpenAI. (2022, July 20). DALL E Now Available in Beta.<br>
+                        <a href="https://openai.com/blog/dall-e-now-available-in-beta" target="_blank">https://openai.com/blog/dall-e-now-available-in-beta</a>
+                    </p>
+                    <p>
+                        Seon. (n.d.). How Do Deepfakes Work?<br>
+                        <a href="https://seon.io/resources/dictionary/deepfakes/#h-how-do-deepfakes-work" target="_blank">https://seon.io/resources/dictionary/deepfakes/#h-how-do-deepfakes-work</a>
+                    </p>
+                    <p>
+                        TechTarget. (2023, April). What is Generative AI? Everything You Need to Know.<br>
+                        <a href="https://www.techtarget.com/searchenterpriseai/definition/Dall-E" target="_blank">https://www.techtarget.com/searchenterpriseai/definition/Dall-E</a>
+                    </p>
+                </div>
             </div>
             <div class="col-md-12">
                 <h3 class="sub-heading">AI Generated vs Human Art</h3>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <div class="gray-rectangle">
+                    <p>
+                        Nanou, E. (2022, September 17). Should AI-Generated Art Be Considered Real Art? MUO.
+                        <a href="https://www.makeuseof.com/is-ai-generated-art-real-art/" target="_blank">https://www.makeuseof.com/is-ai-generated-art-real-art/</a>
+                    </p>
+                </div>
             </div>
             <div class="col-md-12">
                 <h3 class="sub-heading">Copyright and Copywrong</h3>
             </div>
             <div class="col-md-12">
-                <div class="gray-rectangle"></div>
+                <div class="gray-rectangle">
+                <p>
+                    Cartter, E. (2023, March 28). The Pope Francis Puffer Photo Was Real in Our Hearts. GQ.
+                    <a href="https://www.gq.com/story/pope-puffer-jacket-midjourney-ai-meme" target="_blank">https://www.gq.com/story/pope-puffer-jacket-midjourney-ai-meme</a>
+                </p>
+                <p>
+                    Parra, D., & Shroud, S. R. (2023, February 24). The Ethics of AI Art. Center for Media Engagement.
+                    <a href="https://mediaengagement.org/research/the-ethics-of-ai-art/" target="_blank">https://mediaengagement.org/research/the-ethics-of-ai-art/</a>
+                </p>
+                </div>
             </div>
         </div>
     </div>
@@ -149,5 +235,9 @@ export default defineComponent({
     margin: 5px 0; /* Add margin to paragraphs within the rectangle */
     text-align: left;
     font-size: 15px;
+}
+
+a {
+    color: black; /* Change this to your desired link color */
 }
 </style>
