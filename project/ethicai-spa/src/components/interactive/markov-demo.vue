98d2848ca611292295e0cf0d9c486ef8637dda44
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-09-08 19:24:28 +1200

    Created Markov Chain Demo component

---
+++ project/ethicai-spa/src/components/interactive/markov-demo.vue
@@ -0,0 +1,99 @@
+<template>
+    <form class="form-container d-flex justify-content-center align-items-center flex-wrap">
+        <div class="form-group">
+            <textarea v-model="inputPromptText" class="form-control" id="exampleFormControlTextarea1" rows="3"
+                placeholder="Enter your input here" :disabled="loading"></textarea>
+            <div class="d-flex justify-content-end pt-2">
+                <div v-if="loading" class="spinner-grow" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+                <send-btn @button-clicked="handleButtonClick" :disable-button="loading"></send-btn>
+            </div>
+        </div>
+        <div class="form-group">
+            <textarea v-model="outputPromptText" class="form-control" id="exampleFormControlTextarea1" rows="3"
+                disabled></textarea>
+        </div>
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import sendBtn from '../reusable-ui/send-btn.vue';
+export default defineComponent({
+    name: 'MarkovDemo',
+    components: {
+        'send-btn': sendBtn
+    },
+    props: {
+    },
+    setup() {
+        const inputPromptText = ref('');
+        const outputPromptText = ref('')
+        const loading = ref(false);
+
+        const handleButtonClick = () => {
+            const requestBody = JSON.stringify({ prompt: inputPromptText.value });
+
+            inputPromptText.value = '';
+            outputPromptText.value = ''
+            loading.value = true;
+
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: requestBody,
+            };
+
+            fetch('https://oruwgbr24mmcdfjr5efi5qxel40vodks.lambda-url.ap-southeast-2.on.aws/', requestOptions)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    outputPromptText.value = data.response;
+                    loading.value = false;
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    loading.value = false;
+                });
+        };
+
+        return {
+            loading,
+            handleButtonClick,
+            inputPromptText,
+            outputPromptText
+        }
+    }
+});
+</script>
+
+<style scoped>
+/* Add your CSS styles here, scoped to this component */
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap');
+
+.form-container {
+    border: 2px solid #E8E4E4;
+    box-shadow: 5px 5px 5px #E8E4E4;
+    border-radius: 20px;
+}
+
+.form-group {
+    width: 60%;
+}
+
+.form-control {
+    border: 2px solid black;
+    border-radius: 10px;
+}
+
+.spinner-grow {
+    background-color: #BF8FFE;
+}
+</style>
