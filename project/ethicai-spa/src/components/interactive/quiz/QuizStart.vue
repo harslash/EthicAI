ec466c80b945c54b77711485203c130716772fb5
Kevin Wong <kevinxhwong@gmail.com>
2023-09-10 23:03:40 +1200

    tweaked start button on quiz

--- project/ethicai-spa/src/components/interactive/quiz/QuizStart.vue
+++ project/ethicai-spa/src/components/interactive/quiz/QuizStart.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="mx-auto p-16 bg-gray-50 lg:max-w-[60%]">
-    <h1 class="text-center my-10 p-2 text-3xl">Quiz Review</h1>
+  <div class="grid place-items-center mx-auto p-16 bg-gray-50 lg:max-w-[60%]">
+    <h1 class="text-center my-5 p-2 text-4xl">Quiz Review</h1>
     <div :style="{ backgroundColor: '#6D0CFF' }" @click="$emit('quiz-started')"
-      class="flex justify-center items-center min-h-[85px] cursor-pointer rounded-xl hover:scale-105 transition-all duration-150">
+      class="flex justify-center items-center w-80 min-h-[85px] cursor-pointer rounded-xl hover:scale-105 transition-all duration-150">
       <p class="text-2xl text-center top-1/2 text-white">Start</p>
     </div>
   </div>
