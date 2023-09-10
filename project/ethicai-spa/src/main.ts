614bc5ad261fd9e5c7e2e3c2bc182fc0bcc090f5
Kevin Wong <kevinxhwong@gmail.com>
2023-09-10 22:26:22 +1200

    fixed tailwind implementation

--- project/ethicai-spa/src/main.ts
+++ project/ethicai-spa/src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router'
 
 //styling
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'primevue/resources/themes/saga-blue/theme.css'; // Choose a theme
