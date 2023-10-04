77aa4dd935cc5d008ef1bb1458f170c7d016f3d9
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-10-04 21:19:06 +1300

    Updated content in chatbots module page 1

--- project/ethicai-spa/src/components/pages/chatbots/chatbots-page-1.vue
+++ project/ethicai-spa/src/components/pages/chatbots/chatbots-page-1.vue
@@ -11,16 +11,18 @@
         <div class="col-md-8 d-flex align-items-center">
           <div class="d-flex justify-content-center flex-wrap">
             <p class="text-left section-text">
-              At their humble beginning, chatbots were primitive computer programs that mimicked human intelligence by answering questions with scripted responses. In 1966, ELIZA was created. ELIZA repeated words and phrases the user had typed, by inserting them into a list of pre-written phrases. At the time, some users of that rudimentary system were convinced they were conversing with a fellow human.
+            Let's delve into the past when chatbots were in their infancy. These early computer programs had a modest ambition: to simulate human intelligence by offering pre-scripted responses to questions. In the year 1966, a notable chatbot named ELIZA was born. ELIZA's unique approach involved reusing the user's input in a set of predefined phrases. Surprisingly, this basic system often left users believing they were engaged in a genuine human conversation. 
             </p>
 
             <p class="section-text">
-              In 2011, Apple released the revolutionary voice assistant, Siri, with more advanced capabilities than any chatbot predecessors offering. Since then, the popularity of chatbots has drastically surged due to their convenience and flexibility (<router-link :to="{ name: 'references' }">Ina, 2023</router-link>). Nowadays, people use chatbots to provide customer service, assist in various tasks, practice language skills, and more.
+           Fast forward to 2011, and Apple introduced Siri, a voice assistant that pushed the boundaries of chatbot capabilities. Siri's advanced features marked a significant turning point. Since then, chatbots have gained widespread popularity due to their user-friendly interface, simplifying interactions with computers         (<router-link :to="{ name: 'references' }">Ina, 2023</router-link>). Today, chatbots find utility across a spectrum of tasks, from customer service to assisting with everyday activities and language practice, among other applications.
             </p>
           </div>
         </div>
-        <div class="col-md-4 d-flex align-items-center">
-          <img class="img-a" src="../../../assets//chatbots_page_1_a.jpeg" alt="2d illustration of chatbots"/>
+
+        <div class="col-md-4 d-flex align-items-center justify-content-center">
+          <a title="File:ELIZA conversation.jpg, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:ELIZA_conversation.png">
+            <img width="400" alt="ELIZA conversation" src="https://upload.wikimedia.org/wikipedia/commons/7/79/ELIZA_conversation.png"></a>
         </div>
       </div>
       <div class="row h-30 mb-">
@@ -30,10 +32,10 @@
         <div class="col-md-8 d-flex align-items-center">
           <div class="d-flex justify-content-center flex-wrap">
             <p class="section-text">
-              The latest development of chatbots is large language models or LLMs. LLMs can perform a range of text-based tasks, like summarizing, translating, and generating content, using information gained from large training datasets of text downloaded from all over the internet. You've probably heard of ChatGPT, an LLM-based chatbot that can have a human-like dialogue with a user.
+              The world of chatbots has witnessed a remarkable evolution with the advent of Large Language Models (LLMs). These sophisticated systems have become a game-changer, as they can handle a wide range of text-based tasks. Imagine tasks like summarizing, translating, and generating content with the help of information gathered from vast datasets of online text. One LLM-based chatbot that might ring a bell is ChatGPT. It's a chatbot that can chat with you in a way that feels remarkably human.             
             </p>
             <p class="section-text">
-              Since LLMs arrived on the scene, chatbots such as ChatGPT and Google Bard have taken the world by storm. These chatbots seem to know everything and are conveniently accessible to anyone with an internet connection. Thanks to this, ChatGPT is now heavily used to answer questions, to assist in writing, to create written content, and more.
+             Since the rise of LLMs, chatbots like ChatGPT and Google Bard have truly captured the spotlight. These chatbots appear to have an uncanny knowledge of the world and, best of all, they're readily available to anyone with an internet connection. Thanks to this accessibility, ChatGPT has become a go-to tool for answering questions, lending a hand with writing tasks, creating content, and much more.
             </p>
           </div>  
         </div>
@@ -43,7 +45,7 @@
           <purple-btn
                 v-if="!section1Completed"
                 :text="'Continue'"
-                @click="showSecondSection = true; scrollIntoSecondSection(); section1Completed = true;"
+                @click="showSecondSection = true; scrollIntoSection('secondSection'); section1Completed = true;"
                 />
         </div>
       </div>
@@ -55,23 +57,24 @@
       text-section" 
       :class="{ 'hidden': !showSecondSection }"
     >
-      <div class="row h-40">
-        <div class="col-md-12 pb-2">
-          <h5>Have a go for yourself!</h5>
-          <span class="gpt-prompt-note">Note: This demo is powered by the OpenAssistant model from Hugging Face. Visit https://chat.openai.com/ to experiment with the official ChatGPT model.</span>
-        </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-8">            
-          <chatgpt-prompt-form class="form"></chatgpt-prompt-form>
-        </div>
-        <div class="col-md-2"></div>
-      </div>
+    <div class="row">
+        <div class="col-md-12">
+          <div class="d-flex justify-content-center">
+            <p class="section-text">
+                Here's a glimpse into ChatGPT's inner workings. ChatGPT undergoes comprehensive training by drawing data from diverse sources on the internet, including Wikipedia, books, news articles, and scientific journals. After this immersive learning process, ChatGPT is primed for interaction. As showcased in our flowchart, you initiate interaction by sending ChatGPT a prompt or question. ChatGPT, armed with the knowledge it has acquired during training, then crafts a tailored response, promptly delivering it back to you, the user.
+              </p>
+          </div>
+       </div>
+       <div class="col-md-12">
+          <img src="../../../assets//chatbots_page_1_a.png" class="img-a" alt="flowchat diagram explaing the interaction between a user and ChatGPT"/>
+       </div>
+    </div>
       <div class="row h-20">
         <div class="col-md-12 d-flex justify-content-end">
           <purple-btn
                 v-if="section1Completed && !section2Completed"
                 :text="'Continue'"
-                @click="showThirdSection = true; scrollIntoThirdSection(); section2Completed = true;"
+                @click="showThirdSection = true; scrollIntoSection('thirdSection'); section2Completed = true;"
                 />
         </div>
       </div>
@@ -79,52 +82,27 @@
     <div 
         ref="thirdSection"
         class="section-three container text-section pb-5" 
-        :class="{ 'hidden': !showThirdSection }"
-        >
-        <div class="row h-20 mb-3">
-            <div class="col-md-12 pb-5">
-            <h5 class="pb-4">Is this your first time using a LLM?</h5>
-            <br>
-            <div class="radio-div d-flex justify-content-around">
-                <div class="form-check">
-                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                <label class="form-check-label" for="flexRadioDefault1">
-                    Yes
-                </label>
-                </div>
-                <div class="form-check">
-                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
-                <label class="form-check-label" for="flexRadioDefault2">
-                    No
-                </label>
-                </div>
+        :class="{ 'hidden': !showThirdSection }">
+        <div class="row h-40">
+          <div class="col-md-12 pb-2">
+            <div class="d-flex align-items-center flex-column">
+              <h5>Engage with ChatGPT</h5>
+              <p class="section-text">Welcome to our ChatGPT interaction feature. You can interact in two ways:</p>
+              <p class="section-text px-3">1. Your Questions: Enter your own questions or prompts in the text box and receive responses from ChatGPT.</p>
+              <p class="section-text px-3">2. Pre-Generated Prompts: Choose from a selection of pre-made prompts to start a conversation.</p>
+
+              <p class="section-text">Feel free to explore and experiment. Your curiosity is the key to discovering ChatGPT's capabilities. Enjoy the interaction!</p>
+              <span class="gpt-prompt-note">Note: This demo is powered by the OpenAssistant model from Hugging Face. Visit https://chat.openai.com/ to experiment with the official ChatGPT model.</span>
             </div>
-            </div>
-            <div class="col-md-12">
-            <h5 class="pb-5">What is your trust level in ChatGPT?</h5>
-            <p> (Drag slider to answer) </p>
-            <br>
-            <div class="d-flex justify-content-center">
-                <div class="slider-container">
-                <prime-slider v-model="value" class="w-14rem mb-2" :step="25"/>
-                <div class="d-flex justify-content-between">
-                    <span class="slider-label">None</span>
-                    <span class="slider-label">Low</span>
-                    <span class="slider-label">Neutral</span>
-                    <span class="slider-label">Moderate</span>
-                    <span class="slider-label">High</span>
-                </div>
-                </div>
-            </div>
-            <br>
-            <p
-                v-if="value !== null && value >= 0 && showThanksMessage"
-                class="purple-text"
-            >
-                Thanks for your input!
-            </p>
-            </div>
-            <!-- Move the thanks message here -->
+           
+          </div>
+        </div>
+        <div class="row pb-5">
+          <div class="col-md-2"></div>
+          <div class="col-md-8">            
+            <chatgpt-prompt-form class="form"></chatgpt-prompt-form>
+          </div>
+          <div class="col-md-2"></div>
         </div>
         <div class="row h-20">
             <div class="col-md-12 d-flex justify-content-between">
@@ -194,22 +172,14 @@ export default defineComponent({
     },
   },
   methods: {
-    scrollIntoSecondSection() {   
+    scrollIntoSection(sectionName: string) {   
       this.$nextTick(() => {
-        const element = this.$refs.secondSection as HTMLElement;
+        const element = this.$refs[sectionName] as HTMLElement;
         if (element) {
           element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
         }
       }); 
     },
-    scrollIntoThirdSection() {
-      this.$nextTick(() => {
-        const element = this.$refs.thirdSection as HTMLElement;
-        if (element) {
-          element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
-        }
-      }) 
-    }
   },
   components: {
     'nav-bar': NavBar,
@@ -226,11 +196,6 @@ export default defineComponent({
 
 .container-fluid.page-container {
   margin-top: 60px;
-}
-
-.img-a {
-  width: 75%;
-  height: 75%;
 }
 
 .section-text {
@@ -280,5 +245,13 @@ export default defineComponent({
   color: #6D0CFF;
   transition: color 2s ease-in-out;
 }
+
+@media (max-width: 767.98px) {
+  .section-text {
+    width: 90%;
+  }
+}
+
+
 
 </style>
