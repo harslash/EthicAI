520389a2a2dd748cb8e75265bf123741084f16cc
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-10-05 15:07:46 +1300

    Updated footer content

--- project/ethicai-spa/src/components/reusable/page-footer.vue
+++ project/ethicai-spa/src/components/reusable/page-footer.vue
@@ -4,20 +4,25 @@
       <div class="col-md-8 p-5">
         <img class src="../../assets/logo-bland.png" width="220"/>
       </div>
-      <div class="col-md-4 p-5">
-        <h4 class="mb-3">Company</h4>
+      <div class="col-md-2 p-5">
+        <h4 class="mb-3">Product</h4>
         <ul class="footer-list">
           <li class="mb-2">
             <router-link to="/references">
                 <a href="#">References</a>
             </router-link>
           </li>
-          <li class="mb-2">
-              <router-link to="/about-us">
-                <a href="#">About Us</a>
-              </router-link>
-          </li>
         </ul>
+      </div>
+      <div class="col-md-2 p-5">
+           <h4 class="mb-3">Company</h4>
+           <ul class="footer-list">
+              <li class="mb-2">
+                  <router-link to="/about-us">
+                    <a href="#">About Us</a>
+                  </router-link>
+              </li>
+          </ul>
       </div>
     </div>
   </div>
@@ -62,7 +67,7 @@ export default defineComponent({
 
   @media (max-width: 767.98px) {
     .container-fluid  {
-      height: 400px;
+      height: 510px;
     }
   }
 </style>
