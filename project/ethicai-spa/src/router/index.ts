4b007b78b9fad98ea32502bec24e7c1fd4e4780e
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-10-05 15:32:56 +1300

    Created resources page

--- project/ethicai-spa/src/router/index.ts
+++ project/ethicai-spa/src/router/index.ts
@@ -16,13 +16,18 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ReferencesView.vue'),
   },
-   {
+  {
     path: '/about-us',
     name: 'about-us',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutUsView.vue'),
+  },
+   {
+    path: '/resources',
+    name: 'resources',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ResourcesView.vue'),
   },
   {
     path: '/chatbots',
---
+++ project/ethicai-spa/src/views/ResourcesView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="resources">
+        <resources-page></resources-page>
+    </div>
+</template>
+
+<script lang="ts">
+import ResourcesPage from '../components/pages/resources-page.vue'
+export default {
+    name: 'ResourcesView',
+    components: {
+        'resources-page': ResourcesPage
+    }
+}
+</script>
