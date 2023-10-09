0a3e8d66836c93819a3fa9db0a3af31fd84d53cf
Kevin Wong <kevinxhwong@gmail.com>
2023-10-10 00:03:01 +1300

    fixed line spaces

--- project/ethicai-spa/src/components/pages/deepfakes/deepfakes-page-3.vue
+++ project/ethicai-spa/src/components/pages/deepfakes/deepfakes-page-3.vue
@@ -1,22 +1,22 @@
 <template>
-    <nav-bar />
-    <div class="container-fluid page-container text-center">
-        <div class="container mb-5 text-section">
-            <div class="row h-10 mb-5">
-                <div class="col-md-12">
-                    <h1 class="mt-5">Real or Not Real?</h1>
-                </div>
-            </div>
-            <div class="col-md-12 d-flex justify-content-between">
-                <router-link to="/deepfakes/ai-for-evil">
-                    <purple-btn-outline :text="'Back'"></purple-btn-outline>
-                </router-link>
-                <router-link to="/deepfakes/deepfakes-in-media">
-                    <purple-btn :text="'Continue'" @click="handlePageCompletionClick()"></purple-btn>
-                </router-link>
-            </div>
+  <nav-bar />
+  <div class="container-fluid page-container text-center">
+    <div class="container mb-5 text-section">
+      <div class="row h-10 mb-5">
+        <div class="col-md-12">
+          <h1 class="mt-5">Real or Not Real?</h1>
         </div>
+      </div>
+      <div class="col-md-12 d-flex justify-content-between">
+        <router-link to="/deepfakes/ai-for-evil">
+          <purple-btn-outline :text="'Back'"></purple-btn-outline>
+        </router-link>
+        <router-link to="/deepfakes/deepfakes-in-media">
+          <purple-btn :text="'Continue'" @click="handlePageCompletionClick()"></purple-btn>
+        </router-link>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -26,21 +26,21 @@ import PurpleBtn from '../../reusable-ui/purple-btn.vue';
 import PurpleBtnOutline from '../../reusable-ui/purple-btn-outline.vue';
 
 export default defineComponent({
-    name: 'DeepfakesPage3',
-    data() {
-        return {
-        };
-    },
-    methods: {
-        handlePageCompletionClick(this: any) {
-            this.$registerPageAsCompleted('deepfakes', 'real-or-not-real');
-        }
-    },
-    components: {
-        'nav-bar': NavBar,
-        'purple-btn': PurpleBtn,
-        'purple-btn-outline': PurpleBtnOutline,
+  name: 'DeepfakesPage3',
+  data() {
+    return {
+    };
+  },
+  methods: {
+    handlePageCompletionClick(this: any) {
+      this.$registerPageAsCompleted('deepfakes', 'real-or-not-real');
     }
+  },
+  components: {
+    'nav-bar': NavBar,
+    'purple-btn': PurpleBtn,
+    'purple-btn-outline': PurpleBtnOutline,
+  }
 });
 </script>
 
@@ -48,29 +48,29 @@ export default defineComponent({
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap');
 
 .container-fluid.page-container {
-    margin-top: 60px;
+  margin-top: 60px;
 }
 
 .section-text {
-    width: 60%;
-    text-align: left;
+  width: 60%;
+  text-align: left;
 }
 
 .text-section {
-    opacity: 1;
-    max-height: 2000px;
-    transition: max-height 0.5s ease-in-out, opacity 0.5s ease-in-out;
+  opacity: 1;
+  max-height: 2000px;
+  transition: max-height 0.5s ease-in-out, opacity 0.5s ease-in-out;
 }
 
 .hidden {
-    opacity: 0;
-    max-height: 0;
-    overflow: hidden;
+  opacity: 0;
+  max-height: 0;
+  overflow: hidden;
 }
 
 @media (max-width: 767.98px) {
-    .section-text {
-        width: 90%;
-    }
+  .section-text {
+    width: 90%;
+  }
 }
 </style>
