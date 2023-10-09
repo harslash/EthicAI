673bfe74f3529f14b1fe44f94a882ee67fa0e01a
Kevin Wong <kevinxhwong@gmail.com>
2023-10-10 11:03:45 +1300

    added page tracking to chatbots module

--- project/ethicai-spa/src/components/pages/chatbots/chatbots-page-2.vue
+++ project/ethicai-spa/src/components/pages/chatbots/chatbots-page-2.vue
@@ -2,79 +2,89 @@
     <nav-bar />
     <div class="container-fluid page-container text-center">
         <div class="container mb-5 text-section" :class="{ 'hidden': !showFirstSection }">
-                <div class="row h-10 mb-5">
-                    <div class="col-md-12">
-                        <h1 class="mt-5">Too Good to be True?</h1>
+            <div class="row h-10 mb-5">
+                <div class="col-md-12">
+                    <h1 class="mt-5">Too Good to be True?</h1>
+                </div>
+            </div>
+            <div class="row h-30 mb-5">
+                <div class="col-md-12 pb-5">
+                    <div class="d-flex justify-content-center flex-wrap">
+                        <p class="section-text">
+                            These new AI chatbots may be impressive, but their use can lead to many problems. The users like
+                            yourself need to understand how they function and their implications.
+                        </p>
+                        <p class="section-text">
+                            The current most popular chatbot, ChatGPT, works by attempting to understand your prompt and
+                            spitting out strings of words that it predicts will be the best response based on the data used
+                            to train it (<router-link :to="{ name: 'references' }">Guinness, 2023</router-link>).
+                        </p>
                     </div>
                 </div>
-                <div class="row h-30 mb-5">
-                    <div class="col-md-12 pb-5">
-                        <div class="d-flex justify-content-center flex-wrap">
-                            <p class="section-text">
-                                These new AI chatbots may be impressive, but their use can lead to many problems. The users like yourself need to understand how they function and their implications.
-                            </p>
-                            <p class="section-text">
-                                The current most popular chatbot, ChatGPT, works by attempting to understand your prompt and spitting out strings of words that it predicts will be the best response based on the data used to train it (<router-link :to="{ name: 'references' }">Guinness, 2023</router-link>).
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                          <img class="img-a" src="../../../assets//chatbots_page_2_a.jpeg" alt="2d illustration of a robot pulling messages from a box"/>
-                    </div>
+                <div class="col-md-12">
+                    <img class="img-a" src="../../../assets//chatbots_page_2_a.jpeg"
+                        alt="2d illustration of a robot pulling messages from a box" />
                 </div>
-                  <div class="row h-20">
-                    <div class="col-md-12 d-flex justify-content-end">
-                        <purple-btn
-                        v-if="!section1Completed"
-                        :text="'Continue'"
-                        @click="showSecondSection = true; scrollIntoSecondSection(); section1Completed = true;"
-                        />
-                    </div>
+            </div>
+            <div class="row h-20">
+                <div class="col-md-12 d-flex justify-content-end">
+                    <purple-btn v-if="!section1Completed" :text="'Continue'"
+                        @click="showSecondSection = true; scrollIntoSecondSection(); section1Completed = true;" />
                 </div>
+            </div>
         </div>
-         <div
-            ref="secondSection"
-            class="container mb-5 text-section" :class="{ 'hidden': !showSecondSection }">
-                    <div class="row h-30 mb-5">
-                        <div class="col-md-12 pb-5">
-                            <div class="d-flex justify-content-center flex-wrap">
-                                <p class="section-text">
-                                    The creators of ChatGPT have trained the model on an extensive collection of textual data containing over 570 GB of data (<router-link :to="{ name: 'references' }">Ruby, 2023</router-link>). That's over 1.3 million novels worth of information! The source of this data includes books, articles, and web pages.
-                                </p>
-                                <p class="section-text">
-                                    If you’ve been on the internet for very long, you might expect some of those sources of data to contain biases, stereotypes, and prejudiced language. ChatGPT unintentionally learns these biases and may produce offensive responses or reinforce harmful stereotypes when it replies to prompts. The training data used to create ChatGPT and other LLMs, can at times result in biassed or inappropriate responses (<router-link :to="{ name: 'references' }">Ray, 2023</router-link>).
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                                <img class="img-a" src="../../../assets//chatbots_page_2_b.jpeg" alt="2d illustration of a robot pulling messages from a box"/>
-                        </div>
+        <div ref="secondSection" class="container mb-5 text-section" :class="{ 'hidden': !showSecondSection }">
+            <div class="row h-30 mb-5">
+                <div class="col-md-12 pb-5">
+                    <div class="d-flex justify-content-center flex-wrap">
+                        <p class="section-text">
+                            The creators of ChatGPT have trained the model on an extensive collection of textual data
+                            containing over 570 GB of data (<router-link :to="{ name: 'references' }">Ruby,
+                                2023</router-link>). That's over 1.3 million novels worth of information! The source of this
+                            data includes books, articles, and web pages.
+                        </p>
+                        <p class="section-text">
+                            If you’ve been on the internet for very long, you might expect some of those sources of data to
+                            contain biases, stereotypes, and prejudiced language. ChatGPT unintentionally learns these
+                            biases and may produce offensive responses or reinforce harmful stereotypes when it replies to
+                            prompts. The training data used to create ChatGPT and other LLMs, can at times result in biassed
+                            or inappropriate responses (<router-link :to="{ name: 'references' }">Ray, 2023</router-link>).
+                        </p>
                     </div>
-                        <div class="row h-20">
-                            <div class="col-md-12 d-flex justify-content-end">
-                                <purple-btn
-                                v-if="section1Completed && !section2Completed"
-                                :text="'Continue'"
-                                @click="showThirdSection = true; scrollIntoThirdSection(); section2Completed = true;"
-                                />
-                            </div>
-                        </div>
+                </div>
+                <div class="col-md-12">
+                    <img class="img-a" src="../../../assets//chatbots_page_2_b.jpeg"
+                        alt="2d illustration of a robot pulling messages from a box" />
+                </div>
+            </div>
+            <div class="row h-20">
+                <div class="col-md-12 d-flex justify-content-end">
+                    <purple-btn v-if="section1Completed && !section2Completed" :text="'Continue'"
+                        @click="showThirdSection = true; scrollIntoThirdSection(); section2Completed = true;" />
+                </div>
+            </div>
         </div>
-         <div
-            ref="thirdSection"
-            class="container text-section pb-5"
-            :class="{ 'hidden': !showThirdSection }">
+        <div ref="thirdSection" class="container text-section pb-5" :class="{ 'hidden': !showThirdSection }">
             <div class="row h-30 pb-5">
                 <div class="col-md-12 pb-5">
                     <div class="d-flex justify-content-center flex-wrap">
                         <p class="section-text">
-                            Let's see how biases in a text can influence how language models response. The demo below allows you to train a ‘Markov chain’ model on some training data, then see the kind of output it produces. Just like ChatGPT, Markov chains can generate sentences. They both learn statistical relationships between words from their training data, then they can complete sentences by predicting the most likely next word in the text. However, Markov chains are much simpler than the system behind ChatGPT, and you’ll see a direct correspondence between the training corpus and what the Markov chain generates.
+                            Let's see how biases in a text can influence how language models response. The demo below allows
+                            you to train a ‘Markov chain’ model on some training data, then see the kind of output it
+                            produces. Just like ChatGPT, Markov chains can generate sentences. They both learn statistical
+                            relationships between words from their training data, then they can complete sentences by
+                            predicting the most likely next word in the text. However, Markov chains are much simpler than
+                            the system behind ChatGPT, and you’ll see a direct correspondence between the training corpus
+                            and what the Markov chain generates.
                         </p>
                         <p class="section-text">
-                            Pay attention to the red highlighting: it shows biassed phrases that can be carried through into the output of the model. This is similar to what goes on when ChatGPT repeats biassed ideas it learnt from the internet.
+                            Pay attention to the red highlighting: it shows biassed phrases that can be carried through into
+                            the output of the model. This is similar to what goes on when ChatGPT repeats biassed ideas it
+                            learnt from the internet.
                         </p>
                         <p class="section-text">
-                            Experiment for yourself, by training the Markov chain model on each corpus of training data, and consider the relationship between biases in the input and output.
+                            Experiment for yourself, by training the Markov chain model on each corpus of training data, and
+                            consider the relationship between biases in the input and output.
                         </p>
                     </div>
                 </div>
@@ -83,7 +93,9 @@
                 </div>
                 <div class="col-md-12 d-flex justify-content-center flex-wrap">
                     <p class="section-text">
-                        As you can see from the demo, biases in ChatGPT's training data can cause it to produce harmful responses. Some are obvious, such as overt racial prejudices, and some are less visible, such as morality biases (<router-link :to="{ name: 'references' }">Cousins, 2023</router-link>).
+                        As you can see from the demo, biases in ChatGPT's training data can cause it to produce harmful
+                        responses. Some are obvious, such as overt racial prejudices, and some are less visible, such as
+                        morality biases (<router-link :to="{ name: 'references' }">Cousins, 2023</router-link>).
                     </p>
                     <p class="section-text">
                         <b>Lesson of the day: </b>ChatGPT would be a lousy lawmaker.
@@ -96,7 +108,7 @@
                         <purple-btn-outline :text="'Back'"></purple-btn-outline>
                     </router-link>
                     <router-link to="/chatbots/hallucination">
-                        <purple-btn :text="'Continue'"></purple-btn>
+                        <purple-btn :text="'Continue'" @click="handlePageCompletionClick()" />
                     </router-link>
                 </div>
             </div>
@@ -131,7 +143,7 @@ export default defineComponent({
                 if (element) {
                     element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
                 }
-        });
+            });
         },
         scrollIntoThirdSection() {
             this.$nextTick(() => {
@@ -140,6 +152,9 @@ export default defineComponent({
                     element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
                 }
             });
+        },
+        handlePageCompletionClick(this: any) {
+            this.$registerPageAsCompleted('chatbots', 'too-good-to-be-true');
         },
     },
     components: {
@@ -168,20 +183,19 @@ export default defineComponent({
 }
 
 .text-section {
-  opacity: 1;
-  max-height: 1000px;
-  transition: max-height 0.5s ease-in-out, opacity 0.5s ease-in-out;
+    opacity: 1;
+    max-height: 1000px;
+    transition: max-height 0.5s ease-in-out, opacity 0.5s ease-in-out;
 }
 
 .hidden {
-  opacity: 0;
-  max-height: 0;
-  overflow: hidden;
+    opacity: 0;
+    max-height: 0;
+    overflow: hidden;
 }
 
 .demo {
     height: 75vh;
     width: 80%;
 }
-
 </style>
--- project/ethicai-spa/src/components/pages/chatbots/chatbots-page-3.vue
+++ project/ethicai-spa/src/components/pages/chatbots/chatbots-page-3.vue
@@ -1,98 +1,97 @@
 <template>
     <nav-bar />
     <div class="container-fluid page-container text-center">
-       <div class="container mb-5 text-section" :class="{ 'hidden': !showFirstSection }">
-                    <div class="row h-10 mb-5">
-                        <div class="col-md-12">
-                            <h1 class="mt-5">Hallucination</h1>
-                        </div>
+        <div class="container mb-5 text-section" :class="{ 'hidden': !showFirstSection }">
+            <div class="row h-10 mb-5">
+                <div class="col-md-12">
+                    <h1 class="mt-5">Hallucination</h1>
+                </div>
+            </div>
+            <div class="row h-30 mb-5">
+                <div class="col-md-12 pb-5">
+                    <div class="d-flex justify-content-center flex-wrap">
+                        <p class="section-text">
+                            Can an AI hallucinate? Yes, but not in the way we normally think of that word. ChatGPT often
+                            generates confident, plausible-sounding responses that are just blatantly incorrect or
+                            nonsensical. This behaviour is called hallucination, a critical flaw in existing
+                            publicly-available LLMs. OpenAI, the company that developed ChatGPT, have repeatedly warned
+                            users about ChatGPT’s hallucinations, which they admit are a challenging issue to overcome
+                            (<router-link :to="{ name: 'references' }">OpenAI, n.d.</router-link>).
+                        </p>
                     </div>
-                    <div class="row h-30 mb-5">
-                        <div class="col-md-12 pb-5">
-                            <div class="d-flex justify-content-center flex-wrap">
-                                <p class="section-text">
-                                    Can an AI hallucinate? Yes, but not in the way we normally think of that word. ChatGPT often generates confident, plausible-sounding responses that are just blatantly incorrect or nonsensical. This behaviour is called hallucination, a critical flaw in existing publicly-available LLMs. OpenAI, the company that developed ChatGPT, have repeatedly warned users about ChatGPT’s hallucinations, which they admit are a challenging issue to overcome (<router-link :to="{ name: 'references' }">OpenAI, n.d.</router-link>).
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                              <img class="img-a" src="../../../assets/chatbots_page_3_a.jpeg" alt="2d illustration of a robot pulling messages from a box"/>
-                        </div>
-                    </div>
-                      <div class="row h-20">
-                        <div class="col-md-12 d-flex justify-content-end">
-                            <purple-btn
-                            v-if="!section1Completed"
-                            :text="'Continue'"
-                            @click="showSecondSection = true; scrollIntoSecondSection(); section1Completed = true;"
-                            />
-                        </div>
-                    </div>
-        </div>
-        <div
-            ref="secondSection"
-            class="container mb-5 text-section" :class="{ 'hidden': !showSecondSection }">
-                    <div class="row h-30 mb-5">
-                        <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
-                            <div class="section-text">
-                                An example can be seen below:
-                            </div>
-                        </div>
-                        <div class="col-md-12 d-flex justify-content-center pb-5">
-                            <chatgpt-ui-card
-                                :questionText="questionText"
-                                :answerText="answerText"
-                                ></chatgpt-ui-card>
-                        </div>
-                        <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
-                                <div class="section-text">
-                                In fact, all months have four Tuesdays. However, with reasoning that seems convincing, ChatGPT could fool trusting users into accepting false information. As a user of the chatbot, you are responsible for confirming the accuracy of ChatGPT's responses. This is especially important when dealing with information on sensitive topics such as legal knowledge, medical treatments and industry compliance requirements. If users unquestioningly trust information from ChatGPT in those areas, there may be severe real-world consequences.
-                                </div>
-                        </div>
-                    <div class="row h-20">
-                        <div class="col-md-12 d-flex justify-content-end">
-                            <purple-btn
-                            v-if="section1Completed && !section2Completed"
-                            :text="'Continue'"
-                            @click="showThirdSection = true; scrollIntoThirdSection(); section2Completed = true;"
-                            />
-                        </div>
-                    </div>
+                </div>
+                <div class="col-md-12">
+                    <img class="img-a" src="../../../assets/chatbots_page_3_a.jpeg"
+                        alt="2d illustration of a robot pulling messages from a box" />
+                </div>
+            </div>
+            <div class="row h-20">
+                <div class="col-md-12 d-flex justify-content-end">
+                    <purple-btn v-if="!section1Completed" :text="'Continue'"
+                        @click="showSecondSection = true; scrollIntoSecondSection(); section1Completed = true;" />
+                </div>
             </div>
         </div>
-        <div
-            ref="thirdSection"
-            class="container mb-5 text-section" :class="{ 'hidden': !showThirdSection }">
-                    <div class="row h-30 mb-5">
-                        <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
-                            <div class="section-text">
-                                Do you have what it takes to judge ChatGPT's responses for accuracy? Look at each generated response from ChatGPT, and see if the response is factually correct.
-                            </div>
-                        </div>
-                        <div class="col-md-12 pb-5">
-                         <hallucination-demo @end-of-demo-event="handleEndOfDemoEvent"></hallucination-demo>
-                        </div>
+        <div ref="secondSection" class="container mb-5 text-section" :class="{ 'hidden': !showSecondSection }">
+            <div class="row h-30 mb-5">
+                <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
+                    <div class="section-text">
+                        An example can be seen below:
+                    </div>
+                </div>
+                <div class="col-md-12 d-flex justify-content-center pb-5">
+                    <chatgpt-ui-card :questionText="questionText" :answerText="answerText"></chatgpt-ui-card>
+                </div>
+                <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
+                    <div class="section-text">
+                        In fact, all months have four Tuesdays. However, with reasoning that seems convincing, ChatGPT could
+                        fool trusting users into accepting false information. As a user of the chatbot, you are responsible
+                        for confirming the accuracy of ChatGPT's responses. This is especially important when dealing with
+                        information on sensitive topics such as legal knowledge, medical treatments and industry compliance
+                        requirements. If users unquestioningly trust information from ChatGPT in those areas, there may be
+                        severe real-world consequences.
+                    </div>
+                </div>
+                <div class="row h-20">
+                    <div class="col-md-12 d-flex justify-content-end">
+                        <purple-btn v-if="section1Completed && !section2Completed" :text="'Continue'"
+                            @click="showThirdSection = true; scrollIntoThirdSection(); section2Completed = true;" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div ref="thirdSection" class="container mb-5 text-section" :class="{ 'hidden': !showThirdSection }">
+            <div class="row h-30 mb-5">
+                <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
+                    <div class="section-text">
+                        Do you have what it takes to judge ChatGPT's responses for accuracy? Look at each generated response
+                        from ChatGPT, and see if the response is factually correct.
+                    </div>
+                </div>
+                <div class="col-md-12 pb-5">
+                    <hallucination-demo @end-of-demo-event="handleEndOfDemoEvent"></hallucination-demo>
+                </div>
             </div>
         </div>
 
         <div ref="lastSection" class="container text-section" :class="{ 'hidden': !showLastSection }">
             <div class="row">
-                 <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
+                <div class="col-md-12 d-flex flex-wrap justify-content-center pb-5">
                     <div class="section-text">
                         <b>Lesson of the day:</b> Don’t ask ChatGPT to do your maths homework.
                     </div>
                 </div>
-            <div class="row h-20 pb-5">
-                <div class="col-md-12 d-flex justify-content-between">
-                    <router-link to="/chatbots/too-good-to-be-true">
-                        <purple-btn-outline :text="'Back'"></purple-btn-outline>
-                    </router-link>
-                    <router-link to="/chatbots/misuses-and-plagiarism">
-                        <purple-btn :text="'Continue'"></purple-btn>
-                    </router-link>
+                <div class="row h-20 pb-5">
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <router-link to="/chatbots/too-good-to-be-true">
+                            <purple-btn-outline :text="'Back'"></purple-btn-outline>
+                        </router-link>
+                        <router-link to="/chatbots/misuses-and-plagiarism">
+                            <purple-btn :text="'Continue'" @click="handlePageCompletionClick()" />
+                        </router-link>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 </template>
@@ -121,7 +120,7 @@ export default defineComponent({
             section3Completed: false,
         }
     },
-     methods: {
+    methods: {
         scrollIntoSecondSection() {
             this.$nextTick(() => {
                 const element = this.$refs.secondSection as HTMLElement;
@@ -149,7 +148,10 @@ export default defineComponent({
         handleEndOfDemoEvent() {
             this.showLastSection = true;
             this.scrollIntoLastSection();
-        }
+        },
+        handlePageCompletionClick(this: any) {
+            this.$registerPageAsCompleted('chatbots', 'hallucination');
+        },
     },
     components: {
         'nav-bar': NavBar,
@@ -179,14 +181,14 @@ export default defineComponent({
 }
 
 .text-section {
-  opacity: 1;
-  max-height: 1000px;
-  transition: max-height 0.5s ease-in-out, opacity 0.5s ease-in-out;
+    opacity: 1;
+    max-height: 1000px;
+    transition: max-height 0.5s ease-in-out, opacity 0.5s ease-in-out;
 }
 
 .hidden {
-  opacity: 0;
-  max-height: 0;
-  overflow: hidden;
+    opacity: 0;
+    max-height: 0;
+    overflow: hidden;
 }
 </style>
