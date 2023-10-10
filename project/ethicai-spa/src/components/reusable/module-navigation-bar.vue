4af360671a365fd68fd13d9a7485799623c6272b
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-10-10 17:49:35 +1300

    Updated module navigation bar

--- project/ethicai-spa/src/components/reusable/module-navigation-bar.vue
+++ project/ethicai-spa/src/components/reusable/module-navigation-bar.vue
@@ -1,31 +1,130 @@
 <template>
-  <div>
-    <div class="progress">
+  <div class="bar-container container-fluid py-2">
+    <div class="d-flex justify-content-center align-items-center">
       <div
-        class="progress-bar"
-        role="progressbar"
-        style="width: 100%"
-        aria-valuenow="100"
-        aria-valuemin="0"
-        aria-valuemax="100"
-      ></div>
+        class="prev-icon-container d-flex align-items-center justify-content-center"
+      >
+        <i
+          class="fas fa-solid fa-chevron-left prev-icon"
+          style="color: #8b8b8b; font-size: 28px"
+        ></i>
+      </div>
+      <div class="progress mx-3">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          :style="{ width: progressWidth + '%' }"
+          aria-valuenow="100"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        ></div>
+      </div>
+      <div
+        class="next-icon-container d-flex align-items-center justify-content-center"
+      >
+        <i
+          class="fas fa-solid fa-chevron-right next-icon"
+          style="color: #8b8b8b; font-size: 28px"
+        ></i>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, PropType } from "vue";
 
 export default defineComponent({
   name: "ModuleNavigationBar",
-  data() {
-    return {};
+  props: {
+    moduleName: {
+      type: String as PropType<string>,
+      required: true,
+    },
   },
-  methods: {},
+  data() {
+    return {
+      dalleModuleRoutes: [
+        "the-rise-of-dalle",
+        "ai-generated-vs-human-art",
+        "the-complexities-of-copyright",
+        "dalle-quiz",
+        "dalle-complete",
+      ],
+      chatbotsModulePageRoutes: [
+        "the-age-of-chatbots",
+        "too-good-to-be-true",
+        "hallucination",
+        "misuses-and-plagiarism",
+        "chatbot-quiz",
+        "chatbots-complete",
+      ],
+      progressWidth: 0,
+    };
+  },
+  methods: {
+    updateProgress() {
+      const routeName = this.$route.name;
+      const currentPageName: string =
+        routeName !== null && routeName !== undefined
+          ? routeName.toString()
+          : "";
+
+      let currentPageIndex = -1;
+      let newWidth = 0;
+
+      if (this.moduleName == "dalle") {
+        currentPageIndex = this.dalleModuleRoutes.indexOf(currentPageName);
+        newWidth = (currentPageIndex / this.dalleModuleRoutes.length) * 100;
+      }
+
+      if (this.moduleName == "chatbots") {
+        currentPageIndex =
+          this.chatbotsModulePageRoutes.indexOf(currentPageName);
+        newWidth =
+          (currentPageIndex / this.chatbotsModulePageRoutes.length) * 100;
+      }
+
+      this.progressWidth = newWidth;
+    },
+  },
+  mounted() {
+    this.updateProgress();
+  },
 });
 </script>
 
 <style lang="css" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap");
 /* Your component-specific styles go here */
+
+.progress {
+  --bs-progress-bar-bg: #6d0cff;
+  width: 50%;
+}
+
+.bar-container {
+  position: fixed;
+  top: 1;
+  background-color: #ffffff;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+  z-index: 800;
+}
+
+.prev-icon,
+.next-icon {
+  cursor: pointer;
+}
+
+.prev-icon-container,
+.next-icon-container {
+  width: 40px;
+  height: 40px;
+}
+
+.prev-icon-container:hover,
+.next-icon-container:hover {
+  background-color: #cccccc;
+  border-radius: 8px;
+}
 </style>
