c0060e68cec381e1bbe074427c4d598a5f30fa13
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-10-10 21:26:01 +1300

    Added caraousel section in About Us Page

--- project/ethicai-spa/src/components/pages/about-us-page.vue
+++ project/ethicai-spa/src/components/pages/about-us-page.vue
@@ -17,8 +17,206 @@
                 </div>
             </div>
         </div>
+
+        <section class="vh-50 gradient-custom mb-5">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col col-xl-10">
+                        <div class="card">
+                            <div class="card-body py-5">
+
+                                <!-- Carousel wrapper -->
+                                <div id="carouselDarkVariant" class="carousel slide carousel-dark">
+                                <!-- Indicators -->
+                                    <div class="carousel-indicators mb-0">
+                                        <button data-bs-target="#carouselDarkVariant" data-bs-slide-to="0" class="active"
+                                        aria-current="true" aria-label="Slide 1"></button>
+                                        <button data-bs-target="#carouselDarkVariant" data-bs-slide-to="1"
+                                        aria-label="Slide 2"></button>
+                                        <button data-bs-target="#carouselDarkVariant" data-bs-slide-to="2"
+                                        aria-label="Slide 3"></button>
+                                         <button data-bs-target="#carouselDarkVariant" data-bs-slide-to="3"
+                                        aria-label="Slide 4"></button>
+                                         <button data-bs-target="#carouselDarkVariant" data-bs-slide-to="4"
+                                        aria-label="Slide 5"></button>
+                                    </div>
+
+                                    <!-- Inner -->
+                                    <div class="carousel-inner pt-2 pb-5">
+                                        <!-- Single item -->
+                                        <div class="carousel-item active">
+                                            <div class="row d-flex justify-content-center">
+                                                <div class="col-md-8 col-lg-9 col-xl-8">
+                                                <div class="d-flex">
+                                                    <div class="flex-shrink-0">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+                                                        class="rounded-circle mb-4 mb-lg-0 shadow-2" alt="woman avatar" width="90"
+                                                        height="90">
+                                                    </div>
+                                                    <div class="flex-grow-1 ms-4 ps-3">
+                                                    <figure>
+                                                        <blockquote class="blockquote mb-4">
+                                                        <p>
+                                                            <i class="fas fa-quote-left fa-lg text-warning me-2"></i>
+                                                            <span class="font-italic">Lorem ipsum dolor sit amet consectetur adipisicing
+                                                            elit. Pariatur sint nesciunt ad itaque aperiam expedita officiis incidunt
+                                                            minus facere, molestias quisquam impedit inventore.</span>
+                                                        </p>
+                                                        </blockquote>
+                                                        <figcaption class="blockquote-footer">
+                                                            Kevin Wong (Backend Developer)
+                                                        </figcaption>
+                                                    </figure>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Single item -->
+                                        <div class="carousel-item">
+                                            <div class="row d-flex justify-content-center">
+                                                <div class="col-md-8 col-lg-9 col-xl-8">
+                                                <div class="d-flex">
+                                                    <div class="flex-shrink-0">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+                                                        class="rounded-circle mb-4 mb-lg-0 shadow-2" alt="woman avatar" width="90"
+                                                        height="90">
+                                                    </div>
+                                                    <div class="flex-grow-1 ms-4 ps-3">
+                                                    <figure>
+                                                        <blockquote class="blockquote mb-4">
+                                                        <p>
+                                                            <i class="fas fa-quote-left fa-lg text-warning me-2"></i>
+                                                            <span class="font-italic">Sed ut perspiciatis unde omnis iste natus error sit
+                                                            voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                                                            quae ab illo inventore veritatis.</span>
+                                                        </p>
+                                                        </blockquote>
+                                                        <figcaption class="blockquote-footer">
+                                                            Harlean Hansra (Front End Developer)
+                                                        </figcaption>
+                                                    </figure>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Single item -->
+                                        <div class="carousel-item">
+                                            <div class="row d-flex justify-content-center">
+                                                <div class="col-md-8 col-lg-9 col-xl-8">
+                                                <div class="d-flex">
+                                                    <div class="flex-shrink-0">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+                                                        class="rounded-circle mb-4 mb-lg-0 shadow-2" alt="woman avatar" width="90"
+                                                        height="90">
+                                                    </div>
+                                                    <div class="flex-grow-1 ms-4 ps-3">
+                                                    <figure>
+                                                        <blockquote class="blockquote mb-4">
+                                                        <p>
+                                                            <i class="fas fa-quote-left fa-lg text-warning me-2"></i>
+                                                            <span class="font-italic">At vero eos et accusamus et iusto odio dignissimos
+                                                            qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+                                                            et quas molestias excepturi sint amet dolore.</span>
+                                                        </p>
+                                                        </blockquote>
+                                                        <figcaption class="blockquote-footer">
+                                                        Georgia Purkiss (UI Designer)
+                                                        </figcaption>
+                                                    </figure>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                         <!-- Single item -->
+                                        <div class="carousel-item">
+                                            <div class="row d-flex justify-content-center">
+                                                <div class="col-md-8 col-lg-9 col-xl-8">
+                                                <div class="d-flex">
+                                                    <div class="flex-shrink-0">
+                                                    <img src="../../assets/matthew_avatar.jpeg"
+                                                        class="rounded-circle mb-4 mb-lg-0 shadow-2" alt="woman avatar" width="90"
+                                                        height="90">
+                                                    </div>
+                                                    <div class="flex-grow-1 ms-4 ps-3">
+                                                    <figure>
+                                                        <blockquote class="blockquote mb-4">
+                                                        <p>
+                                                            <i class="fas fa-quote-left fa-lg text-warning me-2"></i>
+                                                            <span class="font-italic">I have the privilege to lead the front-end development effort of EthicAI. The most satisfying moment for me in the project was seeing how EthicAI transformed from an initial ideation to a fully functional product.</span>
+                                                        </p>
+                                                        </blockquote>
+                                                        <figcaption class="blockquote-footer">
+                                                            Matthew Kwok (Front End Developer)
+                                                        </figcaption>
+                                                    </figure>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                         <!-- Single item -->
+                                        <div class="carousel-item">
+                                            <div class="row d-flex justify-content-center">
+                                                <div class="col-md-8 col-lg-9 col-xl-8">
+                                                <div class="d-flex">
+                                                    <div class="flex-shrink-0">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+                                                        class="rounded-circle mb-4 mb-lg-0 shadow-2" alt="woman avatar" width="90"
+                                                        height="90">
+                                                    </div>
+                                                    <div class="flex-grow-1 ms-4 ps-3">
+                                                    <figure>
+                                                        <blockquote class="blockquote mb-4">
+                                                        <p>
+                                                            <i class="fas fa-quote-left fa-lg text-warning me-2"></i>
+                                                            <span class="font-italic">At vero eos et accusamus et iusto odio dignissimos
+                                                            qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+                                                            et quas molestias excepturi sint amet dolore.</span>
+                                                        </p>
+                                                        </blockquote>
+                                                        <figcaption class="blockquote-footer">
+                                                            Gali Mortimer-Webster (Content creator)
+                                                        </figcaption>
+                                                    </figure>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Inner -->
+
+                                    <!-- Controls -->
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselDarkVariant"
+                                        data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselDarkVariant"
+                                        data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
+                                </div>
+                                <!-- Carousel wrapper -->
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <page-footer />
     </div>
-    <page-footer />
+
 </template>
 
 <script lang="ts">
@@ -51,5 +249,16 @@ export default defineComponent({
     font-family: 'Open Sans', sans-serif;
     font-weight: 600;
     text-align: center;
+}
+
+.gradient-custom {
+    /* fallback for old browsers */
+    background: #f6d365;
+
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: -webkit-linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
 }
 </style>
