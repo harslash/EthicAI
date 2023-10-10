8e694f91c31dcd2dd49bac756b97270655fec04b
Kevin Wong <kevinxhwong@gmail.com>
2023-10-10 15:04:30 +1300

    updated visuals for about us page

--- project/ethicai-spa/src/components/pages/about-us-page.vue
+++ project/ethicai-spa/src/components/pages/about-us-page.vue
@@ -1,16 +1,24 @@
 <template>
-    <nav-bar/>
+    <nav-bar />
     <div class="container-fluid page-container">
-            <div class="row h-10 mb-5">
-                <div class="col-md-12">
-                    <h1 class="mt-5 heading">About Us</h1>
-
-                    The EthicAI team is a quintet of passionate, competent computer science students in our final year of study at the University of Auckland. We want to see artificial intelligence used responsibly, so we created the EthicAI website to make engaging educational material accessible for all university students to learn about AI. Our site was made in 2023 for the Computer Science 399 course with the support of Danielle Lottridge and Thomas Lacombe, who provided valuable feedback and guidance.
+        <div class="row h-10 mb-5">
+            <div class="col-md-12">
+                <h1 class="mt-5 heading">About Us</h1>
+                <div class="tw-w-3/5 tw-m-auto tw-mt-10">
+                    <p>
+                        The EthicAI team is a quintet of passionate, competent computer science students in our final year
+                        of study at the University of Auckland. We want to see artificial intelligence used responsibly, so
+                        we created the EthicAI website to make engaging educational material accessible for all university
+                        students to learn about AI.<br><br>Our site was made in 2023 for the Computer Science 399 Capstone
+                        Course
+                        with the support of Danielle Lottridge and Thomas Lacombe, who provided valuable feedback and
+                        guidance.
+                    </p>
                 </div>
+            </div>
         </div>
-
     </div>
-    <page-footer/>
+    <page-footer />
 </template>
 
 <script lang="ts">
@@ -35,7 +43,8 @@ export default defineComponent({
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap');
 
 .container-fluid.page-container {
-  margin-top: 60px;
+    margin-top: 60px;
+    height: 90vh;
 }
 
 .heading {
@@ -43,5 +52,4 @@ export default defineComponent({
     font-weight: 600;
     text-align: center;
 }
-
 </style>
