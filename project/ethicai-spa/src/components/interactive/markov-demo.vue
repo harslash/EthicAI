37b49d751398146448d6f859c624970743df72f6
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-10-11 22:30:32 +1300

    Added dropdown for adjusting biases

--- project/ethicai-spa/src/components/interactive/markov-demo.vue
+++ project/ethicai-spa/src/components/interactive/markov-demo.vue
@@ -3,7 +3,7 @@
         <div class="container pb-3">
             <div class="row">
                    <div class="col-md-12 d-flex">
-                    <div class="dropdown px-3">
+                    <div class="dropdown px-2">
                         <button
                             class="btn
                             btn-secondary
@@ -24,8 +24,29 @@
                             </li>
                         </ul>
                     </div>
+                    <div class="dropdown px-2">
+                        <button
+                            class="btn
+                            btn-secondary
+                            dropdown-toggle
+                            demo-btn
+                            " type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Adjust Biases
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item"
+                                    @click="handleAdjustBiasesDropdownClick('original')">{{biasedDropdownItemOneText}}</a>
+                            </li>
+                             <li>
+                                <a class="dropdown-item"
+                                    @click="handleAdjustBiasesDropdownClick('inversesd')">{{biasedDropdownItemTwoText}}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                     <button
-                        class="btn btn-primary demo-btn"
+                        class="btn btn-primary demo-btn mx-2"
                         @click="handleButtonClick"
                         :disabled="loading"
                         type="button">Train Model</button>
@@ -59,7 +80,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, onMounted, computed } from 'vue';
 
 function splitStringToArray(inputString: string, delimiters: string[]) {
     // Split after newlines and spaces
@@ -124,7 +145,7 @@ export default defineComponent({
     },
     setup() {
         const wilsonCorpus = 'I have called the Congress into extraordinary session because there are serious, very serious, choices of policy to be made, and made immediately, which it was neither right nor constitutionally permissible that I should assume the responsibility of making. On the 3rd of February last, I officially laid before you the extraordinary announcement of the Imperial German government that on and after the 1st day of February it was its purpose to put aside all restraints of law or of humanity and use its submarines to sink every vessel that sought to approach either the ports of Great Britain and Ireland or the western coasts of Europe or any of the ports controlled by the enemies of Germany within the Mediterranean...\n\nWhen I addressed the Congress on the 26th of February last, I thought that it would suffice to assert our neutral rights with arms, our right to use the seas against unlawful interference, our right to keep our people safe against unlawful violence. But armed neutrality, it now appears, is impracticable... Armed neutrality is ineffectual enough at best; in such circumstances and in the face of such pretensions it is worse than ineffectual: it is likely only to produce what it was meant to prevent; it is practically certain to draw us into the war without either the rights or the effectiveness of belligerents. There is one choice we cannot make, we are incapable of making: we will not choose the path of submission and suffer the most sacred rights of our nation and our people to be ignored or violated. The wrongs against which we now array ourselves are no common wrongs; they cut to the very roots of human life.\n\nWith a profound sense of the solemn and even tragical character of the step I am taking and of the grave responsibilities which it involves, but in unhesitating obedience to what I deem my constitutional duty, I advise that the Congress declare the recent course of the Imperial German government to be in fact nothing less than war against the government and people of the United States; that it formally accept the status of belligerent which has thus been thrust upon it; and that it take immediate steps, not only to put the country in a more thorough state of defense but also to exert all its power and employ all its resources to bring the government of the German Empire to terms and end the war...\n\nThe world must be made safe for democracy. Its peace must be planted upon the tested foundations of political liberty. We have no selfish ends to serve. We desire no conquest, no dominion. We seek no indemnities for ourselves, no material compensation for the sacrifices we shall freely make. We are but one of the champions of the rights of mankind. We shall be satisfied when those rights have been made as secure as the faith and the freedom of nations can make them...';
-
+        
         const wilsonCorpusBiasedIndices = [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 86, 87, 88, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 171, 172, 173, 174, 175, 176, 191, 192, 193, 194, 240, 241, 242, 243, 244, 245, 250, 251, 260, 262, 312, 313, 319, 320, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 389, 390, 391, 392, 399, 400, 401, 402, 403, 404, 405, 406, 407, 419, 420, 421, 422, 440, 441, 442, 443, 444, 445, 457, 458, 459, 460, 461, 468, 469, 470, 471, 472, 473];
 
         //eslint-disable-next-line
@@ -173,6 +194,18 @@ export default defineComponent({
             }
         };
 
+        const biasedDropdownItemOneText = computed(() => {
+            return corpusLabel.value === "Woodrow Wilson’s 1917 Declaration of War" ? "Biases towards Germany" : "Biases towards women";
+        });
+
+        const biasedDropdownItemTwoText = computed(() => {
+            return corpusLabel.value === "Woodrow Wilson’s 1917 Declaration of War" ? "Biases towards America" : "Biases towards men";
+        });
+
+        const handleAdjustBiasesDropdownClick = (itemId: string) => {
+            console.log(itemId)
+        }
+
          const handleButtonClick = () => {
             const requestBody = JSON.stringify(
                 { corpus: corpusId, context_size: 1, words_to_generate: 100, input: startingWords}
@@ -208,6 +241,9 @@ export default defineComponent({
             loading,
             handleDropdownItemClick,
             handleButtonClick,
+            biasedDropdownItemOneText,
+            biasedDropdownItemTwoText,
+            handleAdjustBiasesDropdownClick,
             corpusLabel,
             corpusCategory
         }
@@ -223,7 +259,7 @@ export default defineComponent({
     border: 2px solid #E8E4E4;
     box-shadow: 5px 5px 5px #E8E4E4;
     border-radius: 20px;
-    height: 600px;
+    height: 580px;
 }
 
 .form-group {
@@ -283,7 +319,17 @@ export default defineComponent({
 .demo-textarea {
     border: 2px solid black;
     border-radius: 10px;
-    height: 150px;
+    height: 180px;
     overflow-y: auto;
+}
+
+.dropdown-item {
+    cursor: pointer;
+}
+
+@media (max-width: 767.98px) {
+   .form-container {    
+        height: 650px;
+    }
 }
 </style>
