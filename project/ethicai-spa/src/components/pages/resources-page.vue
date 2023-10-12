d6a605277e79ec5b64d073533f872d7bcbc16135
Matthew Kwok <79879051+CatOnAMat@users.noreply.github.com>
2023-10-12 15:40:11 +1300

    Updated resources page content

--- project/ethicai-spa/src/components/pages/resources-page.vue
+++ project/ethicai-spa/src/components/pages/resources-page.vue
@@ -6,39 +6,73 @@
                 <h1 class="mt-5 heading">Resources</h1>
             </div>
         </div>
-
+         <div class="row mb-2">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 d-flex justify-content-md-start justify-content-center">
+                <h3 class="card-category-heading">Interactive</h3>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
         <div class="row mb-5">
-            <div class="col-md-12 d-flex justify-content-md-center align-items-center align-items-md-start flex-md-row flex-column">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 d-flex justify-content-md-start align-items-center align-items-md-start flex-md-row flex-column">
                 <div class="card mx-md-5 my-5 my-md-0" style="width: 18rem;">
                     <div class="img-container">
                         <img class="card-img-top" src="http://moralmachine.mit.edu/images/teaser.jpg" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Moral Machine</h5>
+                        <h6 class="card-title">Moral Machine</h6>
                         <p class="card-text">A platform that allows you to gain perspective into the ethical dilemmas behind the moral decisions made by machine intelligence, such as self-driving cars.</p>
                         <a href="https://www.moralmachine.net/"  target="_blank" class="btn btn-primary">Navigate</a>
                     </div>
                 </div>
                 <div class="card mx-md-5 my-5 my-md-0" style="width: 18rem;">
-                    <img class="card-img-top"  alt="Card image cap" src="https://dam-prod2.media.mit.edu/thumb/2023/06/23/IMG_1353-jonathan-williams-edited.jpg.1400x1400.jpg">
+                    <div class="img-container">
+                        <img class="card-img-top"  alt="Card image cap" src="https://dam-prod2.media.mit.edu/thumb/2023/06/23/IMG_1353-jonathan-williams-edited.jpg.1400x1400.jpg">
+                    </div>
                     <div class="card-body">
-                        <h5 class="card-title">Detect Fakes</h5>
-                        <p class="card-text">A platform that allows you to practise trying to detect DeepFakes.</p>
+                        <h6 class="card-title">Detect Fakes</h6>
+                        <p class="card-text">A platform that allows you to practise trying to detect deepfakes.</p>
                         <a href="https://detectfakes.media.mit.edu/"  target="_blank" class="btn btn-primary">Navigate</a>
                     </div>
                 </div>
+            </div>     
+            <div class="col-md-2"></div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 d-flex justify-content-md-start justify-content-center">
+                <h3 class="card-category-heading">Academic</h3>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+
+         <div class="row mb-5">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 d-flex justify-content-md-start align-items-center align-items-md-start flex-md-row flex-column">
                 <div class="card mx-md-5 my-5 my-md-0" style="width: 18rem;">
                     <div class="img-container">
                         <img class="card-img-top" src="https://dl.acm.org/cms/asset/2d78c0fc-794b-4611-bd35-188ebc503257/3313831.cover.jpg" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">AI Literacy</h5>
+                        <h6 class="card-title">AI Literacy</h6>
                         <p class="card-text">This publication addresses the necessity of understanding user competencies for AI interaction and offers insights into designing AI technologies that promote AI literacy, providing a definitive AI literacy definition.</p>
                         <a href="https://dl.acm.org/doi/10.1145/3313831.3376727" target="_blank" class="btn btn-primary">Navigate</a>
                     </div>
                 </div>
+                <div class="card mx-md-5 my-5 my-md-0" style="width: 18rem;">
+                    <div class="img-container">
+                        <img class="card-img-top" src="https://s3.amazonaws.com/coursera_assets/meta_images/generated/XDP/XDP~COURSE!~introduction-to-responsible-ai/XDP~COURSE!~introduction-to-responsible-ai.jpeg" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <h6 class="card-title">Introduction to Responsible AI</h6>
+                        <p class="card-text">This beginner-level online course from Google Cloud aims to explain what responsive AI is, why it's important and how Google incorporates responsive AI into their products.</p>
+                        <a href="https://www.coursera.org/learn/introduction-to-responsible-ai" target="_blank" class="btn btn-primary">Navigate</a>
+                    </div>
+                </div>
             </div>
-           
+            <div class="col-md-2"></div>
         </div>
 
         <page-footer />
@@ -77,6 +111,11 @@ export default defineComponent({
     text-align: center;
 }
 
+.card-category-heading {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 600;
+}
+
 .btn {
     background-color: #6D0CFF;
 }
@@ -85,6 +124,11 @@ export default defineComponent({
     height: 200px;
     overflow: hidden;
 
+}
+
+.card-title {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 700;
 }
 
 .card-text {
