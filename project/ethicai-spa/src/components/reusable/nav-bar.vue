41888cc10e3bc3d1d38538c2e25f9d32874f57f8
GaliM-W <89501387+GaliM-W@users.noreply.github.com>
2023-10-13 13:58:09 +1300

    Fix doubly keyboard focusable elements

--- project/ethicai-spa/src/components/reusable/nav-bar.vue
+++ project/ethicai-spa/src/components/reusable/nav-bar.vue
@@ -1,47 +1,43 @@
 <template>
     <div>
-   <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top" aria-label="Third navbar example">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <router-link to="/">
-                  <img src="../../assets/logo.png" alt="Company Logo" width="130">
-            </router-link>    
-          </a>
-          <button class="navbar-toggler" type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNavDropdown" 
-            aria-controls="navbarNavDropdown" 
-            aria-expanded="false" 
-            aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-          </button>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top" aria-label="Third navbar example">
+            <div class="container-fluid">
 
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav me-auto mb-2 mb-sm-0">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/">
-                        Home
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/all-modules">
-                       Modules
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/resources">
-                        Resources
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                     <router-link class="nav-link" to="/about-us">
-                           About Us
-                    </router-link>
-                </li>  
-            </ul>
-          </div>
-        </div>
-      </nav>
+                <router-link to="/" class="navbar-brand">
+                    <img src="../../assets/logo.png" alt="Company Logo" width="130">
+                </router-link>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/">
+                                Home
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/all-modules">
+                                Modules
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/resources">
+                                Resources
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/about-us">
+                                About Us
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
     </div>
 </template>
@@ -62,6 +58,7 @@ export default defineComponent({
 
 <style lang="css" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap');
+
 /* Your component-specific styles go here */
 .navbar {
     border-bottom: 2px solid #C9C9C9;
@@ -93,6 +90,4 @@ li.nav-item {
     border: 2px solid #6D0CFF;
     color: #6D0CFF;
 }
-
-
 </style>
