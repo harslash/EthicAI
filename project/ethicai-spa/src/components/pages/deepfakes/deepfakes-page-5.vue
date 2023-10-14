63c0b3e188bb2e9d5c7b4bfe77911191f6eec976
georgiapurkiss <104548310+georgiapurkiss@users.noreply.github.com>
2023-10-14 23:43:32 +0000

    deepfakes vids

--- project/ethicai-spa/src/components/pages/deepfakes/deepfakes-page-5.vue
+++ project/ethicai-spa/src/components/pages/deepfakes/deepfakes-page-5.vue
@@ -2,17 +2,56 @@
   <nav-bar />
   <module-navigation-bar :moduleName="'deepfakes'"></module-navigation-bar>
   <div id="main" class="container-fluid page-container text-center">
-    <div class="container mb-5 text-section">
+    <div class="container text-section" :class="{ 'hidden': !showFirstSection }">
       <div class="row h-10 mb-5">
         <div class="col-md-12">
           <h1 class="tw-mt-24">Judgement Day</h1>
         </div>
       </div>
-      <div class="col-md-12 d-flex justify-content-end">
-        <router-link to="/deepfakes/quiz">
-          <purple-btn :text="'Continue'" @click="handlePageCompletionClick()"></purple-btn>
-        </router-link>
+      <div class="row h-30 mb-5">
+        <div class="col-md-12">
+          <div class="d-flex justify-content-center flex-wrap">
+            <p class="section-text">
+              Given a series of videos, you are going to have to judge whether they are a deepfake or not. We recommend you watch at least a minute of the video before making your judgement. Good luck!
+            </p>
+          </div>
+        </div>
       </div>
+
+      <div class="col-md-12 d-flex justify-content-end">
+        <purple-btn :tag="'button'" v-if="!section1Completed" :text="'Start Now'"
+          @click="showSecondSection = true; scrollIntoSection('secondSection'); section1Completed = true;" />
+      </div>
+    </div>
+
+    <div ref="secondSection" class="container row m-auto" :class="{ 'hidden': !showSecondSection }">
+      <div class="row h-30">
+        <div class="col-md-12 pb-1">
+          <div class="d-flex justify-content-center flex-wrap">
+            <p class="section-text text-center">
+              Boris Johnson discusses Peppa Pig world.
+            </p>
+            <div>
+              <iframe width="560" height="315" class="mb-5"
+                src="https://www.youtube.com/embed/8zHURhs0DbM" title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 d-flex justify-content-end">
+        <purple-btn :tag="'button'" v-if="!section2Completed" :text="'Continue'"
+          @click="showThirdSection = true; scrollIntoSection('thirdSection'); section2Completed = true;" />
+      </div>
+    </div>
+
+
+    <div class="col-md-12 d-flex justify-content-end mb-12">
+      <router-link to="/deepfakes/quiz">
+        <purple-btn :text="'Continue'" @click="handlePageCompletionClick()"></purple-btn>
+      </router-link>
     </div>
   </div>
 </template>
@@ -28,9 +67,23 @@ export default defineComponent({
   name: 'DeepfakesPage3',
   data() {
     return {
+      showFirstSection: true,
+      showSecondSection: false,
+      showThirdSection: false,
+      section1Completed: false,
+      section2Completed: false,
+      section3Completed: false,
     };
   },
   methods: {
+    scrollIntoSection(sectionName: string) {
+      this.$nextTick(() => {
+        const element = this.$refs[sectionName] as HTMLElement;
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+        }
+      });
+    },
     handlePageCompletionClick(this: any) {
       this.$registerPageAsCompleted('deepfakes', 'judgement-day');
     }
