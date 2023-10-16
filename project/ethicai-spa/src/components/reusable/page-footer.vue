864dc3b703db121bb2d1d0015e5eea6acd86d4ee
Kevin Wong <kevinxhwong@gmail.com>
2023-10-17 01:33:55 +1300

    updated footer

--- project/ethicai-spa/src/components/reusable/page-footer.vue
+++ project/ethicai-spa/src/components/reusable/page-footer.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="container-fluid">
-     <div class="row">
-      <div class="col-md-8 p-5">
-        <img class src="../../assets/logo-bland.png" width="220" alt="EthicAI logo, no colour"/>
+    <div class="row">
+      <div class="col-md-10 p-5">
+        <img class src="../../assets/logo-bland.png" width="220" alt="EthicAI logo, no colour" />
       </div>
       <div class="col-md-2 p-5">
-        <h4 class="mb-3">Product</h4>
+        <h4 class="mb-3">Project</h4>
         <ul class="footer-list">
           <li class="mb-2">
             <router-link to="/references">
-                References
+              References
+            </router-link>
+          </li>
+          <li class="mb-2">
+            <router-link to="/about-us">
+              About Us
             </router-link>
           </li>
         </ul>
-      </div>
-      <div class="col-md-2 p-5">
-           <h4 class="mb-3">Company</h4>
-           <ul class="footer-list">
-              <li class="mb-2">
-                  <router-link to="/about-us">
-                    About Us
-                  </router-link>
-              </li>
-          </ul>
       </div>
     </div>
   </div>
@@ -43,31 +38,31 @@ export default defineComponent({
 </script>
 
 <style lang="css" scoped>
+.container-fluid {
+  background-color: #6D0CFF;
+  color: #ffffff;
+  height: 300px;
+}
+
+.footer-list {
+  list-style-type: none;
+  margin-left: 0;
+  padding: 0;
+}
+
+.footer-list a {
+  text-decoration: none;
+  color: #ffffff;
+}
+
+.footer-list a:hover {
+  text-decoration: none;
+  color: #b3b4b5;
+}
+
+@media (max-width: 767.98px) {
   .container-fluid {
-    background-color: #6D0CFF;
-    color: #ffffff;
-    height: 300px;
+    height: 510px;
   }
-
-  .footer-list {
-     list-style-type: none;
-     margin-left: 0;
-     padding: 0;
-  }
-
-  .footer-list a {
-    text-decoration: none;
-    color: #ffffff;
-  }
-
-   .footer-list a:hover {
-    text-decoration: none;
-    color: #b3b4b5;
-  }
-
-  @media (max-width: 767.98px) {
-    .container-fluid  {
-      height: 510px;
-    }
-  }
+}
 </style>
